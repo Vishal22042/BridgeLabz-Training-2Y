46ad80599c81ef31c4dfdc48468c0fbb0ae314e8
master <vishalthenua2345@gmail.com>
2025-09-01 16:05:20 +0530

    first commit

---
+++ Workshop/StudentRegistrationSystem.java
@@ -0,0 +1,15 @@
+public class StudentRegistrationSystem {
+
+    public static void displayStudents (String[] students) {
+        System.out.println("Registered Students:");
+        for (String student : students) {
+            System.out.println("- " + student);
+        }
+    }
+
+    public static void compareStrings(String s1, String s2) {
+        System.out.println("Using .equals() : " + s1.equals(s2));
+            System.out.println("Using .equals() : " + s1.equals(s2));
+        }
+    
+    }
